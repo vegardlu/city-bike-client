4f648dac477826143f1bb29b20ee6cdd8d776478
Vegard Hillestad <vegard.hillestad@evry.com>
2021-05-04 23:42:19 +0200

    Removed request parameter that is no longer in use

--- src/main/kotlin/lundeberg/tech/citybike/StationInformationController.kt
+++ src/main/kotlin/lundeberg/tech/citybike/StationInformationController.kt
@@ -9,14 +9,12 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class StationInformationController {
 
     @GetMapping("/stations")
     fun stations(
-        @RequestParam(name = "stationId", required = false) name: String?,
         model: Model,
         @Value("\${endpoint.stationstatus}") stationStatusEndpoint: String,
         @Value("\${endpoint.stationinformation}") stationInformationEndpoint: String
